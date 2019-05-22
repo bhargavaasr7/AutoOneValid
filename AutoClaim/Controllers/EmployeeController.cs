94d962eae619af0d11351abf4435865c6b739c5e
bhargava sankarapu <bhargavaasr7@outlook.com>
2019-05-22 10:14:40 +0530

    upto login validate

--- AutoClaim/Controllers/EmployeeController.cs
+++ AutoClaim/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoClaim.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,44 @@ namespace AutoClaim.Controllers
 {
     public class EmployeeController : Controller
     {
+        private ApplicationDbContext dbContext = null;
+        public EmployeeController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            dbContext.Dispose();
+        }
         // GET: Employee
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Register   register)
+        {
+            var user = dbContext..SingleOrDefault(c => c.UserName == registeredUser.UserName && c.Password == registeredUser.Password);
+            if (user!=null)
+            {
+                if (user.UserName.StartsWith("AS"))
+                {
+                    return RedirectToAction("Index", "Assessor");
+                }
+               else if (user.UserName.StartsWith("CO"))
+                {
+                    return RedirectToAction("Index", "ClaimOfficer");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Employee");
+            }
             return View();
         }
     }
---
+++ AutoClaim/Models/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoClaim.Models
+{
+    public class Register
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(50,ErrorMessage ="UserName Can't exceed morethan 50 characters")]
+        [Display(Name ="UserName")]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [StringLength(8,ErrorMessage ="Password length should be 8")]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+    }
+}
